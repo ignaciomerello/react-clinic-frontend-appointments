e7d916c8999a2dbce4e594c6ed5ead9586916fd7
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-11-02 13:18:10 +0100

    Profile not displaying User Data

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -4,45 +4,55 @@ import axios from 'axios';
 const Profile = () => {
 
 
-  const [dataUser, setUser] = useState([]);//Hook
+    const [dataUser, setUser] = useState([]);//Hook
 
-  useEffect(() => {
+    useEffect(() => {
 
-      axios.get('https://clinic-appointments-mongodb.herokuapp.com/user')
-          .then((res) => {
-              console.log(res.data);
-              setUser(res.data);
+        axios.get('https://clinic-appointments-mongodb.herokuapp.com/user')
+            .then((res) => {
+                console.log(res.data);
+                setUser(res.data);
 
-              
 
-          }).catch((err) => {
-              console.log(err);
-          });
 
-  }, []);
+            }).catch((err) => {
+                console.log(err);
+            });
 
-  const userData = (data) => {
-      //console.log(appointment.title);
-      let storage = JSON.parse(localStorage.setItem("User"));
-      console.log(storage);
-  }
+    }, []);
 
-  return (
-      <div className="container">
-          <div className="title">
-              HERE IS YOUR PERSONAL DATA
+    const userData = (data) => {
+        //console.log(appointment.title);
+        let storage = JSON.parse(localStorage.setItem("User"));
+        console.log(storage);
+    }
+
+    return (
+        <div className="container">
+            <div className="title">
+                HERE IS YOUR PERSONAL DATA
               </div>
+            <div className="appointmentContainer">
+                {dataUser.map(Users =>
+                    <div className="cardAppointment" key={Users._id} onClick={() =>
+                        userData(Users)}>{Users.username}
+                        <br></br>
+                        {Users.email}
+                    </div>
+                )}
+            </div>
+            {/* 
           <div className="appointmentContainer">
-              {dataUser.map(Users =>
-                  <div className="cardAppointment" key={Users._id} onClick={() =>
-                      userData(Users)}>{Users.username}
+              {dataUser.map(storage =>
+                  <div className="cardAppointment" key={storage._id} onClick={() =>
+                      userData(storage)}>{storage.username}
                       <br></br>
-                      {Users.email}
+                      {storage.email}
                   </div>
               )}
-          </div>
-      </div>
-  );
+          </div> */}
+        </div>
+    );
 
 }
 
